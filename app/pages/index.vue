04b12cb8be36806f621cee851300aaf4ded2b4b7
Ружилов Артем <ruzhilov.a@promail366.com>
2025-11-18 17:21:10 +0300

    есть загрузка новостей

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>Home</div>
+  <div>
+    <div>loading = {{ loading }}</div>
+    <div>error = {{ error }}</div>
+    <div>articles = {{ articles }}</div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
+import type { NewsArticleDto } from '~/api/generated';
+import { useApi } from '~/api/useApi';
+
+const loading = ref(true);
+const error = ref<string | null>(null);
+const articles = ref<NewsArticleDto[]>([]);
+
+const api = useApi();
+
+const loadNews = async () => {
+  loading.value = true;
+  error.value = null;
+  articles.value = await api
+    .getArticles()
+    .catch((err) => {
+      error.value = err.message || "Ошибка при загрузке новостей";
+      return [];
+    })
+    .finally(() => (loading.value = false));
+};
+
+onMounted(() => loadNews());
 </script>
 
 <style scoped>
